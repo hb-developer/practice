dc80bd9a3c288a26f34c10d1a00df9144b444dd8
hb-developer <dev.hongbin@gmail.com>
2021-07-12 17:40:10 +0900

    search_header

--- React/youtube-clone/src/app.jsx
+++ React/youtube-clone/src/app.jsx
@@ -1,25 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import './app.css';
-import VideoList from './components/video_list/video_list';
+import React, { useEffect, useState } from "react";
+import styles from "../src/app.module.css";
+import SearchHeader from "./components/search_header/search_header";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
   const [videos, setVideos] = useState([]);
 
   useEffect(() => {
     const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
+      method: "GET",
+      redirect: "follow",
     };
 
     fetch(
-      'https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAtrSZkopq--QXlpEYQ5SrM9Kg5TZlZMl0',
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAtrSZkopq--QXlpEYQ5SrM9Kg5TZlZMl0",
       requestOptions
     )
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
   }, []);
-  return <VideoList videos={videos} />;
+  return (
+    <div className={styles.app}>
+      <SearchHeader />
+      <VideoList videos={videos} />;
+    </div>
+  );
 }
 
 export default App;
