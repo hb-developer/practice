8de17d87d014642359b9560cb9e2608c37227256
hb-developer <dev.hongbin@gmail.com>
2021-07-17 16:50:56 +0900

    youtube detail

--- React/youtube-clone/src/app.jsx
+++ React/youtube-clone/src/app.jsx
@@ -1,22 +1,45 @@
-import React, { useEffect, useState } from "react";
-import styles from "../src/app.module.css";
-import SearchHeader from "./components/search_header/search_header";
-import VideoList from "./components/video_list/video_list";
+import React, { useEffect, useState } from 'react';
+import styles from './app.module.css';
+import VideoList from './components/video_list/video_list';
+import SearchHeader from './components/search_header/search_header';
+import VideoDetail from './components/video_detail/video_detail';
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
-  const Search = (query) => {
-    youtube.search(query).then((items) => setVideos(items));
+  const selectVideo = video => {
+    setSelectedVideo(video);
+  };
+
+  const search = query => {
+    youtube
+      .search(query) //
+      .then(videos => setVideos(videos));
   };
 
   useEffect(() => {
-    youtube.mostPopular().then((videos) => setVideos(videos));
+    youtube
+      .mostPopular() //
+      .then(videos => setVideos(videos));
   }, []);
   return (
     <div className={styles.app}>
-      <SearchHeader onSearch={Search} />
-      <VideoList videos={videos} />;
+      <SearchHeader onSearch={search} />
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? 'list' : 'grid'}
+          />
+        </div>
+      </section>
     </div>
   );
 }
--- React/youtube-clone/src/service/youtube.js
+++ React/youtube-clone/src/service/youtube.js
@@ -2,29 +2,27 @@ class Youtube {
   constructor(key) {
     this.key = key;
     this.getRequestOptions = {
-      method: "GET",
-      redirect: "follow",
+      method: 'GET',
+      redirect: 'follow',
     };
   }
-  search(query) {
-    return fetch(
-      `https://www.googleapis.com/youtube/v3/search?key=AIzaSyD7Yr7cl7AZTJxyBb77ZN7huDPYxT4ls68&part=snippet&maxResults=25&q=${query}&key=AIzaSyD7Yr7cl7AZTJxyBb77ZN7huDPYxT4ls68`,
+
+  async mostPopular() {
+    const response = await fetch(
+      `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
       this.getRequestOptions
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({ ...item, id: item.id.videoId }))
-      )
-      .then((items) => items);
+    );
+    const result = await response.json();
+    return result.items;
   }
 
-  mostPopular() {
-    return fetch(
-      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAtrSZkopq--QXlpEYQ5SrM9Kg5TZlZMl0",
+  async search(query) {
+    const response = await fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
       this.getRequestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => result.items);
+    );
+    const result = await response.json();
+    return result.items.map(item => ({ ...item, id: item.id.videoId }));
   }
 }
 
