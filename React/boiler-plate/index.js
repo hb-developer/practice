89482b47f93ca548e1bb5eb12bea8b741e91dab6
hb-developer <dev.hongbin@gmail.com>
2021-07-21 18:16:43 +0900

    비밀 정보 보호

--- React/boiler-plate/index.js
+++ React/boiler-plate/index.js
@@ -1,24 +1,36 @@
 const express = require("express");
-
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+const config = require("./config/key");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://hongbin:abcd1234@cluster0.gzvvc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("mongodb connected..."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false }, err);
+    return res.status(200).json({ success: true });
+  });
 });
 
 app.listen(port, () => {
